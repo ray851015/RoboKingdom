03c98b64e1afe591e489804d672618f0ff4c2c95
ray851015 <ray851015@gmail.com>
2021-07-31 13:52:25 +0800

    Yohooo!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import Cardlist from "./Cardlist";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
 import "./index.css";
+import ErrorBoundry from "./Error";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -22,14 +27,20 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <hr />
-        <Cardlist robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <hr />
+          <ErrorBoundry>
+            <Cardlist robots={filteredRobots} />
+          </ErrorBoundry>
+        </div>
+      );
+    }
   }
 }
 
